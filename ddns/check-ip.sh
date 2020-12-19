0a246b0db1a4445ebcc4a4760d1225e72a7ec310
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2020-12-19 00:55:31 -0600

    added different IP checks

--- ddns/check-ip.sh
+++ ddns/check-ip.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+# LAN IP
+#IP_ADDRESS=$(ifconfig wlan0 | grep "inet " | awk -F'[: ]+' '{ print $3 }')
+
 # Current IP address and path to IP cache file
 #IP_ADDRESS=`dig +short myip.opendns.com @resolver1.opendns.com`
-IP_ADDRESS=$(ifconfig wlan0 | grep "inet " | awk -F'[: ]+' '{ print $3 }')
+#IP_ADDRESS=$(host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}')
+IP_ADDRESS=$(curl ifconfig.co)
 CACHE_PATH="/etc/ddns/cache.txt"
+# More examples found here: https://www.tecmint.com/find-linux-server-public-ip-address/
 
 # Fetch last value of IP address sent to server or create cache file
 if [ ! -f $CACHE_PATH ]; then touch $CACHE_PATH; fi
