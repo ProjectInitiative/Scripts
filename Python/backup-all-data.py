3c4895c18bdbf8e35f82b4c089c61e5a91b6e4a8
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2021-01-24 23:39:37 -0600

    adding in basic version and help menu

--- Python/backup-all-data.py
+++ Python/backup-all-data.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 
-import argparse
+import argparse, logging, os, pathlib
 from datetime import datetime
 
+__version__ = '0.0.1'
 
 def main():
     
+
+    # parse all incoming arguments
+    parser = argparse.ArgumentParser(description='Program to manage, rotate, and upload data to a remote')
+    parser.add_argument('--version',
+                        action='version',
+                        version='%(prog)s {version}'.format(version=__version__))
+
+    # parser.add_argument('integers', metavar='N', type=int, nargs='+',
+    #                     help='an integer for the accumulator')
+    # parser.add_argument('--sum', dest='accumulate', action='store_const',
+    #                     const=sum, default=max,
+    #                     help='sum the integers (default: find the max)')
+
+    args = parser.parse_args()
+
+    # check to make sure correct dependencies are installed
     all_tools_installed = True
     if not is_tool("borg"):
         all_tools_installed = False
@@ -27,6 +44,6 @@ def is_tool(name):
     return which(name) is not None
 
 
-if __name__ == "main":
+if __name__ == '__main__':
     main()
 
